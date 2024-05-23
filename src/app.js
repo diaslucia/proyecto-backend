16ff4f1b7166452055a8420a2e5f191757bb9ef4
diaslucia <luciadias.dev@gmail.com>
2024-05-22 23:44:07 -0300

    UPDATE config

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import routes from "./routes/index.routes.js";
 import __dirname from "./dirname.js";
+import handlebars from "express-handlebars";
 import { Server } from "socket.io";
 
 const app = express();
@@ -9,16 +10,21 @@ const PORT = 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Handlebars
+
+app.engine("handlebars", handlebars.engine());
+app.set("views", __dirname + "/views");
+app.set("view engine", "handlebars");
+app.use(express.static(__dirname + "/public"));
+// Routes
+app.use("/api", routes);
+/* app.use("/static", express.static("public")); */
+
+// Sockets
+
 const httpServer = app.listen(8080, () => {
   console.log(`Escuchando puerto ${PORT}`);
 });
-
-// Routes
-
-app.use("/api", routes);
-app.use("/static", express.static("public"));
-
-// Sockets
 
 let products = [];
 
@@ -26,6 +32,10 @@ const socketServer = new Server(httpServer);
 
 socketServer.on("connection", (socket) => {
   console.log("connected");
+
+  socket.on("message", (data) => {
+    console.log(data);
+  });
 
   socket.on("product", (data) => {
     products.push(data);
--- src/routes/views.routes.js
+++ src/routes/views.routes.js
@@ -3,7 +3,12 @@ import { Router } from "express";
 const router = Router();
 
 router.get("/", (req, res) => {
+  const person = {
+    name: "Juan",
+  };
+
   res.render("index", { styles: "styles.css" });
+  res.render("index", person);
 });
 
 export default router;
